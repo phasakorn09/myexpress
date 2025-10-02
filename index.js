94477f9c528097a16ba821841419000603f33dc7
phasakorn09 <nudangtop@gmail.com>
2025-10-02 23:48:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new line.Client(config);
 
 // Gemini
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
 
 // --- Middleware และ Routes ---
 app.use('/webhook', line.middleware(config));
